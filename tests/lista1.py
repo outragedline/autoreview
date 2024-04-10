ea4f44200a1b5886c33812e984c1a5d1b74daf95
outragedline <outragedline@proton.me>
2024-04-10 13:59:08 -0300

    Improve the example test a little

--- tests/lista1.py
+++ tests/lista1.py
@@ -2,18 +2,20 @@ from autoreview.lista import Lista
 import pexpect
 
 
-def atv01Impl(binary: str, n1: str, n2: str, output: str):
+def atv01Impl(binary: str, n1: int, n2: int):
     child = pexpect.spawn(binary)
-    child.sendline(n1)
-    child.sendline(n2)
-    child.expect(output)
+    child.sendline(f"{n1}")
+    child.sendline(f"{n2}")
+    if n1 + n2 > 10:
+        child.expect(f"{n1 + n2}")
 
 
 class Lista1(Lista):
     def atv01(self, binary: str):
-        atv01Impl(binary, "12", "12", "24")
-        atv01Impl(binary, "1", "1", "24")
-        atv01Impl(binary, "2", "2", "190231")
+        atv01Impl(binary, 12, 12)
+        atv01Impl(binary, 10, 10)
+        atv01Impl(binary, 5, 6)
+        atv01Impl(binary, 1, 1)
 
 
 lista1 = Lista1("./test_samples/")
